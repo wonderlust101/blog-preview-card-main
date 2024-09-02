70c3bc7f914a2ab9419cc92b3bff7b18d2505851
Sergei Borja <sergei.borja0107@gmail.com>
2024-09-02 14:03:24 -0600

    Fixed location errors.

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,7 +1,5 @@
 import React, {ReactNode} from "react";
 import CardContent from "./CardContent.tsx";
-import profilePicture from '/src/assets/images/image-avatar.webp';
-import cardImage from '/src/assets/images/illustration-article.svg';
 
 type CardProps = {
     children?: ReactNode;
@@ -14,12 +12,12 @@ type CardProps = {
 export default function Card({children, title, date, tag, author}: CardProps): React.JSX.Element {
     return (
         <section className='card'>
-            <img className="card__image" src={cardImage} alt="" aria-hidden={true}/>
+            <img className="card__image" src='/images/illustration-article.svg' alt="" aria-hidden={true}/>
             <CardContent title={title} date={date} tag={tag}>
                 {children}
             </CardContent>
             <div className="card__author">
-                <img className="card__author-image" src={profilePicture} alt=""/>
+                <img className="card__author-image" src='/images/image-avatar.webp' alt=""/>
                 <p className="card__author--bold">{author}</p>
             </div>
         </section>)
