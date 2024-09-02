29ad06a8ebd0df7e16f7d81a037e178f4a0f46a9
Sergei Borja <sergei.borja0107@gmail.com>
2024-09-02 14:24:09 -0600

    Fixed font error.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './scss/main.scss'
+import './assets/scss/main.scss'
 
 
 createRoot(document.getElementById('root')!).render(
