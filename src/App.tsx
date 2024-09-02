a5c41f7bcfc13c7f4373032b24580eabf442310e
Sergei Borja <sergei.borja0107@gmail.com>
2024-09-02 13:36:06 -0600

    Converted project to react project.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Card from './components/card/Card.tsx';
+
+function App() {
+
+  return (
+    <>
+      <Card title='HTML & CSS foundations' date='21 Dec 2023' tag='Learning' author='Greg Hooper'>
+          These languages are the backbone of every website, defining structure, content, and presentation.
+      </Card>
+    </>
+  )
+}
+
+export default App
